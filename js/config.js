7bc387fa372ab61246a0232690be91b23999670c
Ricardo <ricardo@onra.com.br>
2026-08-10 23:37:25 -0300

    Configure Google Sheets Webhook URL

--- js/config.js
+++ js/config.js
@@ -7,5 +7,5 @@ window.ONRA_CONFIG = {
   whatsappNumber: "5527988055172",
   contactEmail: "",
   canonicalBase: "https://onra.com.br/",
-  googleSheetWebhookUrl: "" // COLOQUE A URL DO APP DA WEB AQUI APÓS CRIAR O GOOGLE APPS SCRIPT
+  googleSheetWebhookUrl: "https://script.google.com/macros/s/AKfycbwRX4PYSmJmFlJ6amsn01MAeBjNvoOVM7p-fFCSv5BcuSGHBt-QGcn8hbQaIydmJ1YB/exec"
 };
